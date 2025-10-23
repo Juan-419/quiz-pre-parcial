7da8d2c1d1fe309b0a356f5deaf8806d81dc6ccb
Sala 9 Equipo 15 <S09E15@ucatolica.loc>
2025-10-23 10:31:50 -0500

    seg mod

--- models.py
+++ models.py
@@ -11,10 +11,11 @@ class bus (SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     placa: str
     conductor_id: int = Field(foreign_key="conductor.id")
-    conductor: Optional[Bus] = Relationship(back_populates="bus")
+    conductor: Optional[bus] = Relationship(back_populates="bus")
 
 
 class destino (SQLModel, table=True):
     id: int =(default=None, primary_key=True)
     lugar: str
     bus_id: int = Field(foreign_key="bus.id")
+    bus: Optional[destino] = Relationship(back_populates:"destino")
