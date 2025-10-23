ddb3774dfae61b17a91f1c42f946be202d9dde19
Sala 9 Equipo 15 <S09E15@ucatolica.loc>
2025-10-23 10:17:22 -0500

    Modificacion archivos

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from db import create_db_and_tables
 from bus import router as bus_router
 from destino import router as destino_router
-
+from conductor import router as conductor_router
 
 app = FastAPI(title="Transportes sigmotoa")
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+app.include_router(bus_router, prefix="/Bus", tags=["Buses"])
+app.include_router(destino_router, prefix="/Destino", tags=["Destino"])
+app.include_router(conductor_router, prefix="/Conductor", tags=["Conductor"])
--- models.py
+++ models.py
@@ -1,7 +1,13 @@
 from sqlmodel import Field, SQLModel
 
+class conductor(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    nombre: str
+    buses: List["Bus"] = Relationship(back_populates="conductor")
 
-class bus (SQLModel, table=True)
-    id: int | None = Field(default=None, primary_key=True)
-    placa:
-    conductor:
+
+
+class bus (SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    placa: str
+    conductor: str = Field (foreign_key="")
