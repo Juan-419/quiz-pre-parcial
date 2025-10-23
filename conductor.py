9d14718cdb6f72e3783ed7e2fe01530e290630f0
Sala 9 Equipo 15 <S09E15@ucatolica.loc>
2025-10-23 10:39:48 -0500

    Router creado del conductor

--- conductor.py
+++ conductor.py
@@ -2,3 +2,19 @@ from fastapi import APIRouter
 from sqlmodel import select
 from models import conductor
 from utils import get_session
+
+router = APIRouter()
+
+@router.post("/")
+def crear_conductor(Conductor: conductor):
+    with get_session()as session:
+        session.add(Conductor)
+        session.commit()
+        session.refresh(Conductor)
+        return Conductor
+
+@router.get("/")
+def listar_conductor()
+    with get_session() as session:
+        conductor = session.exec(select(conductor)).all()
+        return conductor
